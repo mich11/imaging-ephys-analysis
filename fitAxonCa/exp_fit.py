d306ec3d7719f9762827de4e0f6b788427525454
michelle <michelle.fogerson@gmail.com>
2021-01-28 14:33:55 -0500

    added script and example data

---
+++ fitAxonCa/exp_fit.py
@@ -0,0 +1,82 @@
+'''
+testing whether axonal calcium signals can be 
+approximated with an exponential decay, as for 
+somatic signals. 
+Unitary calcium events were first extracted with 
+wdetecta (cite) and manually curated in clampfit 
+(hence the use of pyabf)
+
+'''
+
+import pyabf
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+def exponential (x,a,b):
+	return a*np.exp(b*x)
+
+fname = "abf/m648_f7_r2dA_curated2.abf"
+abf = pyabf.ABF(fname)
+
+tstart = 12
+tpre = tstart-1
+sx = abf.sweepX*1000
+sweepMat = np.zeros([np.size(sx),abf.sweepCount])
+ymaxpre = np.zeros([abf.sweepCount,1])
+yminpre = np.zeros([abf.sweepCount,1])
+print(abf.sweepCount)
+
+'''
+for each trace, offset and scale
+then average across traces, compute exponential fit,
+and plot average, fit, and individual traces 
+'''
+fig = plt.figure()
+#ax = fig.add_subplot(211)
+ax = plt.subplot2grid((3,1),(0,0),rowspan=2)
+ax2 = plt.subplot2grid((3,1),(2,0))
+
+#ax2 = fig.add_subplot(212)
+
+for stmp in abf.sweepList:
+	abf.setSweep(stmp)
+	preind1 = np.sum((sx-tpre)<0)-1
+	preind2 = np.sum((sx-tstart)<0)-1
+	yoffset = np.mean(abf.sweepY[preind1:preind2])
+	sy = abf.sweepY-yoffset
+
+	ax.plot(sx-tstart, sy, color='0.7')
+	ax.set_xlim(-10,15)
+
+	sweepMat[:,stmp]=sy
+	ymaxpre[stmp]=max(sy[preind1:preind2+34])
+	yminpre[stmp]=min(sy[preind1:preind2+34])
+
+msweep = np.mean(sweepMat,1);
+ymax = max(ymaxpre)*1.25;
+ymin = min(yminpre)*1.5;
+ax.plot(sx-tstart,msweep,color='black')
+ax.set_title(fname)
+ax.set_xlabel('time (s)')
+ax.set_ylim([ymin, ymax])
+
+mmax = np.argmax(msweep[preind2:preind2+8])+preind2
+
+mpost = msweep[mmax:mmax+32]
+xpost = sx[mmax:mmax+32]-tstart
+xpost2 = xpost - min(xpost)
+pars, cov = curve_fit(f=exponential,xdata=xpost2,ydata=mpost,p0=[0,0],bounds=(-np.inf,np.inf))
+stdevs = np.sqrt(np.diag(cov))
+residuals = mpost - exponential(xpost,*pars)
+print(pars)
+tau = 1/pars[1]
+
+ax.plot(xpost,exponential(xpost2,*pars),color='red')
+ax2.set_title('residuals')
+ax2.set_xlabel('time (s)')
+ax2.scatter(xpost,residuals)
+plt.tight_layout()
+
+
+plt.show()
